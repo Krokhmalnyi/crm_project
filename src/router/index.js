c09a98789deb88bf9917622393f0b2b5b54b00ce
Serhii <s.krokhmalnyi@gmail.com>
2022-06-29 18:20:34 +0300

    create 3 pages  with html and style

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,17 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/LoginView',
+    name: 'LoginView',
+    meta:{layout: 'empty'},
+    component: () => import('@/views/LoginView.vue')
+  },
+  {
+    path: '/CategoriesView',
+    name: 'CategoriesView',
+    meta:{layout: 'main'},
+    component: () => import('@/views/CategoriesView.vue')
+      },
 ]
 
 const router = new VueRouter({
