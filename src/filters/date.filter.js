e00d8b934174df7130bca19776b204cd3db7ea79
Serhii <s.krokhmalnyi@gmail.com>
2022-07-03 14:17:02 +0300

    add login and register validation, create plugin

--- src/filters/date.filter.js
+++ src/filters/date.filter.js
@@ -1,4 +1,17 @@
-export default function dateFilter(value){
-    console.log(value) 
-    return value
+export default function dateFilter(value, format = 'date'){
+    const options = {}
+
+    if (format.includes('date')) {
+        options.day = '2-digit'
+        options.month = 'long'
+        options.year =  'numeric'
+    }
+
+    if (format.includes('time')) {
+        options.hour = '2-digit'
+        options.minute = '2-digit'
+        options.second =  '2-digit'
+    }
+
+    return new Intl.DateTimeFormat('uk-Uk', options).format(new Date(value))
 }
